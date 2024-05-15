34ba3763c03c2f440b1ac75c3e8710a32820f157
Timofey <yakubovich96@mail.ru>
2024-05-15 20:23:03 +0300

    enableCors11

--- src/etc/corsOptions.ts
+++ src/etc/corsOptions.ts
@@ -1,7 +1,11 @@
 export const corsOptions = {
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    origin: 'https://arbor-project-client.vercel.app',
-    exposedHeaders: '*',
-    AccessControlAllowOrigin: '*',
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // credentials: true,
+    // origin: 'https://arbor-project-client.vercel.app',
+    // exposedHeaders: '*',
+
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 };
