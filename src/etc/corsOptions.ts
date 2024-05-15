dd7ddbc146767e7855803185b16fd8c9198db035
Timofey <yakubovich96@mail.ru>
2024-05-15 18:55:39 +0300

    enableCors4

--- src/etc/corsOptions.ts
+++ src/etc/corsOptions.ts
@@ -1,5 +1,6 @@
 export const corsOptions = {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
+    origin: 'https://arbor-project-client.vercel.app',
   };
