3f04ee5aaa166941e674bbc9b84886e883958b27
Timofey <yakubovich96@mail.ru>
2024-05-15 17:49:36 +0300

    enableCors3

--- src/etc/corsOptions.ts
+++ src/etc/corsOptions.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
   };
