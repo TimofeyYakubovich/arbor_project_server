153da6910b629b016ff4ad667e6549b4ee6fd18b
Timofey <yakubovich96@mail.ru>
2024-05-15 20:53:55 +0300

    enableCors14

--- src/etc/corsOptions.ts
+++ src/etc/corsOptions.ts
@@ -4,7 +4,7 @@ export const corsOptions = {
     // origin: 'https://arbor-project-client.vercel.app',
     // exposedHeaders: '*',
 
-    "origin": "*",
+    "origin": "https://arbor-project-client.vercel.app",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
